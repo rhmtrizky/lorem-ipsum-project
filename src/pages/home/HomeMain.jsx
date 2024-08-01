5748f181110035c25e85f106076654016217ae21
Ghifani <ghifani03@gmail.com>
2024-08-01 08:13:02 +0700

    create a card

--- src/pages/home/HomeMain.jsx
+++ src/pages/home/HomeMain.jsx
@@ -1,15 +1,19 @@
 import Header from '../../components/ui/Header'
 import React from 'react'
 import HomeBanner from './HomeBanner'
+import HomePoli from './HomePoli'
 
 export default function HomeMain() {
   return (
     <>
         <Header/>
-        <section className='w-full h-ft border-y-2 ' >
+        <section className='w-full h-fit border-y-2 ' >
             <div>
                 <HomeBanner/>
             </div>
+        </section>
+        <section className='m-12' >
+          <HomePoli/>
         </section>
     </>
   )
