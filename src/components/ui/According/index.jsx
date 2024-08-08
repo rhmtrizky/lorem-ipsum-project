4f20c2270df2705c2de488c9fa4c4261c03ba23d
Ghifani <ghifani03@gmail.com>
2024-08-08 17:28:09 +0700

    fix

--- src/components/ui/According/index.jsx
+++ src/components/ui/According/index.jsx
@@ -5,25 +5,29 @@ export default function According({ accorToggle, openIndex }) {
     return (
         <>
             {quetions.map((item, index) => (
-                <div key={item.title} className='w-[650px] bg-white rounded-lg mb-2 shadow-md group'>
+                <div key={item.title} className='max-[545px]:w-full max-[678px]:w-[500px] max-w-[800px] bg-white rounded-lg mr-12 mb-2 shadow-md'>
                     <button 
-                        className='w-full flex items-center px-1 py-2 text-left focus:outline-none focus:border-2 focus:border-[#654ab4] rounded-lg'
+                        className='flex items-center px-1 py-2 text-left focus:outline-none rounded-lg'
                         onClick={() => accorToggle(index)}>
-                        <div className={`w-8 h-8 flex justify-center items-center ${openIndex === index ? 'bg-[#654ab4]' : 'bg-[#6f56bbcc]'} rounded-lg mr-7`}>
+                        <div className={`w-4 h-4 p-4 flex justify-center items-center border-box ${openIndex === index ? 'bg-[#654ab4]' : 'bg-[#6f56bbcc]'} rounded-lg mr-5`}>
                             {openIndex === index 
-                                ? <div className='flex items-center w-[20px] h-[4px] bg-white rounded-full'></div>
+                                ? <div className='flex items-center'>
+                                    <div className=' w-[20px] h-[4px] bg-white rounded-full' ></div>
+                                </div>
                                 : <div className='flex justify-center items-center w-[20px] h-[4px] bg-white rounded-full'>
                                     <div className='w-[20px] h-[5px] bg-white rounded-full' style={{ transform: 'rotate(90deg)' }}></div>
                                    </div>}
                         </div>
-                        <p className='focus:text-red-900 text-[14px]' >{item.title}</p>
+                        <div  >
+                            <p className='focus:text-red-900 text-[14px]' >{item.title}</p>
+                        </div>
                     </button>
                     <motion.div 
                         initial={{ height: 0 }}
                         animate={{ height: openIndex === index ? 'auto' : 0 }}
                         transition={{ duration: 0.3 }}
                         className='overflow-hidden'>
-                        <div className='p-4 border-t border-gray-200'>
+                        <div className='p-4 border-t border-gray-200 '>
                             <p>{item.keterangan}</p>
                         </div>
                     </motion.div>
--- src/components/views/Home/Faq/index.jsx
+++ src/components/views/Home/Faq/index.jsx
@@ -3,6 +3,7 @@ import Header from '@/components/ui/Header'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import According from '@/components/ui/According'
+import BannerContact from '@/components/ui/BannerContact'
 // import styles from '@/styles/Faq.module.css'
 
 export default function Faq() {
@@ -15,25 +16,21 @@ export default function Faq() {
     return (
         <>
             <Header/>
-            <section className='min-h-screen overflow-hidden mb-8'>
-                <div className='flex flex-col justify-center items-center gap-5 bg-flow-regis overflow-hidden'>
-                    <h1 className='text-xl min-[400px]:text-3xl font-bold text-[#fff] cursor-default font-mono'>Pertanyaan Yang Sering Diajukan</h1>
-                    <div className='flex items-center max-[300px]:flex-col max-[400px]:text-xs'>
-                        <a href='#' className='mr-1 mb-1.5 text-white font-semibold cursor-pointer hover:text-[#654AB4]'>Beranda</a>
-                        <Image src={rightArrow} width={14} height={14} alt='right-arrow.png' className='max-[300px]:hidden' />
-                        <p className='text-[#654AB4] blur-text p-1.5 rounded-full ml-2 cursor-default'>Pertanyaan Yang Sering Diajukan</p>
-                    </div>
-                </div>
+            <section className='w-full min-h-screen overflow-hidden mb-8'>
+                <BannerContact 
+                    title={'Pertanyaan Yang Sering Diajukan'}
+                    back={'Beranda'}
+                />
 
-                <div className='flex items-center gap-28 mt-20'>
-                    <div className='relative'>
-                        <Image src={administrationImage} width={700} height={500} alt='administration-image.png' loading='lazy' className='' />
-                        <Image src={dots} width={150} height={200} alt='dots.png' loading='lazy' className='absolute -bottom-20' />
+                <div className='flex max-[1000px]:flex-col min-[1550px]:justify-center items-center mt-10 min-[405px]:mt-20'>
+                    <div className='relative min-[1000px]:mr-12'>
+                        <Image src={administrationImage} width={700} height={500} alt='administration-image.png' loading='lazy' className='max-[400px]:w-[300px] max-[710px]:w-[400px] mx-6' />
+                        <Image src={dots} width={150} height={200} alt='dots.png' loading='lazy' className=' max-[700px]:w-[80px] max-[700px]:-bottom-10 absolute -bottom-20' />
                         <Image src={arrowDottedLeft} width={50} height={20} alt='arro-dotted.png' loading='lazy' className='absolute -top-6 right-0' />
                     </div>
                     
-                    <div className={`h-[90vh] flex flex-col justify-center overflow-y-auto custom `}>
-                        <div className='mt-40 mb-6'>
+                    <div className={` h-[90vh] max-[545px]:mt-10 max-[1000px]:mt-28 max-[545px]:mx-6 max-[1000px]:ml-12 overflow-y-auto custom `}>
+                        <div className='w-full mt-4 mb-6 max-[545px]:text-center '>
                             <p className='text-[#654AB4] cursor-default font-mono mb-1'>Pertanyaan Yang Sering Diajukan</p>
                             <p className='text-4xl font-bold cursor-default font-mono '>Dapatkan Pertanyaan Anda</p>
                         </div>
