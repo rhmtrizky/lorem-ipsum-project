764952229cd64dbcfd0a23f329348cc3a390dae3
Ghifani <ghifani03@gmail.com>
2024-08-10 00:25:15 +0700

    create a modal

--- src/components/ui/Card/CardDoctor/index.jsx
+++ src/components/ui/Card/CardDoctor/index.jsx
@@ -1,20 +1,20 @@
-import { doctorCard, doctorCard1, stetoskopPurple } from '@/assets/images/images'
+import { doctorCard, stetoskopPurple } from '@/assets/images/images'
 import Image from 'next/image'
 import React from 'react'
 
-export default function CardDoctor() {
+export default function CardDoctor({name, spesialisasi}) {
     return (
-        <div className='flex flex-col items-center w-[220px] h-[280px] bg-white shadow-lg rounded-xl p-4' >
+        <div className='flex flex-col items-center w-[220px] h-[290px] bg-white shadow-lg rounded-xl p-4' >
             <div className='w-[170px]' >
                 <Image src={doctorCard} width={200} height={200} alt='doctor.png' className='rounded-lg' />
             </div>
             <div className='mt-3' >
-                <p className='font-bold font-sans' >Dr.Mohammed Darwin Nunez</p>
+                <p className='font-bold font-sans' >{name}</p>
                 <div className='flex gap-2 my-1.5'>
                     <Image src={stetoskopPurple} width={15} height={15} alt='stetoskop.png'/>
-                    <p className='text-xs text-[#654AB4] font-semibold' >Spesialisasi Kandungan</p>
+                    <p className='text-xs text-[#654AB4] font-semibold' >{spesialisasi}</p>
                 </div>
-                <button className='border-2 border-[#654AB4] rounded-full py-1 px-4 text-xs font-semibold mt-3' >Lihat Jadwal</button>
+                <button className='border-2 border-[#654AB4] hover:bg-[#654AB4] hover:text-white rounded-full py-1 px-4 text-xs font-semibold mt-3' >Lihat Jadwal</button>
             </div>
         </div>
     )
--- src/components/views/FindDoctor/index.jsx
+++ src/components/views/FindDoctor/index.jsx
@@ -1,13 +1,49 @@
 import CardDoctor from '@/components/ui/Card/CardDoctor'
+import Filter from '@/components/ui/Filter'
 import Header from '@/components/ui/Header'
 import React from 'react'
 
 export default function FindDoctor() {
+
+
     return (
         <>
             <Header/>
             <section className='m-3 min-h-screen ' >
-                <CardDoctor/>
+                <div>
+                    <h1>Cari Dokter</h1>
+                    <p>Semua dokter berpraktik setiap hari, terdiri dari dokter spesialis, subspesialis sehingga Anda bisa bertemu anggota tim setiap saat. Bekerja secara tim, dari berbagai spesialis multidisplinier, dokter dapat memberikan jalan keluar yang komprehensif untuk setiap masalah yang Anda alami.</p>
+                </div>            
+                <form className='w-full flex gap-2 mb-8' >
+                {/* fungsi sr-only */}
+                <label for="default-search" className="mb-2 text-sm font-medium sr-only">Cari Berdasarkan Nama, dan Subspesialis</label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-5000" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search" className="block w-[500px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-purple-500 focus:border-purple-500 dark:focus:ring-purple-500 dark:focus:border-purple-500 cursor-pointer" placeholder="Cari berdasarkan Nama, dan Spesialis" />
+                </div>
+
+                <Filter/>
+
+                </form>
+
+                <div className='flex flex-wrap gap-3' >
+                    <CardDoctor
+                        name="Dr.Mohamed Ronaldo Brazil"
+                        spesialisasi="Spesialisasi Piala Dunia"
+                    />
+                    {/* <CardDoctor
+                        name="Dr.Darwin Nunez A.A.A"
+                        spesialisasi="Spesialisasi Piala Dunia"
+                    />
+                    <CardDoctor
+                        name="Dr.Setyoko Adi Putra Wahyudi A.A.A DRS S1 Kalimalang"
+                        spesialisasi="Spesialisasi Piala Dunia"
+                    /> */}
+                </div>
             </section>
         </>
     )
