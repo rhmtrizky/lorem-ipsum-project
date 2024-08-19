2fc12c3404a775408df38fc89bdd4e99dfedac3a
Ghifani <ghifani03@gmail.com>
2024-08-19 15:24:11 +0700

    Card family

--- src/components/views/Patient/Dashboard/FamilyView/index.jsx
+++ src/components/views/Patient/Dashboard/FamilyView/index.jsx
@@ -1,4 +1,5 @@
 import PatientDashboardLayout from '@/components/layouts/PatientDashboardLayout'
+import CardFamily from '@/components/ui/Card/CardFamily'
 import React from 'react'
 
 export default function FamilyView() {
@@ -8,8 +9,8 @@ export default function FamilyView() {
                 <div>
                     <h1 className='text-3xl font-bold text-slate-400 font-sans' >Anggota keluarga</h1>   
                 </div>
-                <div>
-
+                <div className="flex flex-wrap gap-2 mt-5 border overflow-y-auto h-[300px] " >
+                    <CardFamily/>
                 </div>
             </div>
         </PatientDashboardLayout>
