e6eaad232933a5ce190f24064a3d4d747b255e0b
Ghifani <ghifani03@gmail.com>
2024-08-31 19:27:15 +0700

    fix card qualified

--- src/components/ui/Card/CardQualifiedDoctors.jsx/index.jsx
+++ src/components/ui/Card/CardQualifiedDoctors.jsx/index.jsx
@@ -1,19 +1,25 @@
-import { doctorCard, stetoskopPurple } from '@/assets/images/images'
-import { datasQualifiedDoctor } from '@/constraint';
+import { stetoskopPurple } from '@/assets/images/images';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Pagination } from 'swiper/modules';
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/free-mode';
 
-export default function CardQualifiedDoctor({name, spesialisasi}) {
+export default function CardQualifiedDoctor({ doctors }) {
+
+    const truncateName = (fullname) => {
+        const maxLength = 40; 
+        if (fullname.length > maxLength) {
+            return fullname.slice(0, maxLength) + '...';
+        }
+        return fullname;
+    };
 
     return (
-
-            <Swiper
-                breakpoints={{
+        <Swiper
+            breakpoints={{
                 300: {
                     slidesPerView: 1,
                     spaceBetween: 15,
@@ -24,38 +30,49 @@ export default function CardQualifiedDoctor({name, spesialisasi}) {
                 },
                 760: {
                     slidesPerView: 2,
-                    spaceBetween: 15
-                }
+                    spaceBetween: 15,
+                },
             }}
             freeMode={true}
             pagination={{
                 clickable: true,
             }}
             modules={[FreeMode, Pagination]}
-            >
-                    {
-                        datasQualifiedDoctor.map((data, index) => (
-                            <SwiperSlide key={index} >
-                                <div  className='flex flex-col w-[220px] h-[290px] bg-white shadow-lg rounded-xl p-4 transform transition duration-500 ease-in-out cursor-pointer my-12'>
-                                    <div className='w-full flex justify-center'>
-                                        <Image src={doctorCard} width={200} height={200} alt='doctor.png' className='rounded-lg' />
-                                    </div>
-                                    <div className='mt-3'>
-                                        <p className='font-bold font-sans'>
-                                            {data.name}
-                                        </p>
-                                        <div className='flex gap-2 my-1.5'>
-                                            <Image src={stetoskopPurple} width={15} height={15} alt='stetoskop.png' />
-                                            <p className='text-xs text-primary font-semibold cursor-default'>{data.specialist}</p>
-                                        </div>
-                                        <button className='border-2 border-primary hover:bg-primary hover:text-white rounded-full py-1 px-4 text-xs font-semibold mt-3 transition duration-[.3s] ease-linear'>
-                                            Lihat Jadwal
-                                        </button>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        ))
-                    }
-            </Swiper>
-    )
+        >
+            {doctors.slice(0, 3).map((doctor, index) => (
+                <SwiperSlide key={index}>
+                    <div className="flex flex-col w-[220px] h-[290px] bg-white shadow-lg rounded-xl p-4 transform transition duration-500 ease-in-out cursor-pointer my-12">
+                        <div className="w-full flex justify-center">
+                            <Image
+                                src={doctor.image}
+                                width={200}
+                                height={200}
+                                alt="doctor.png"
+                                className="rounded-lg"
+                            />
+                        </div>
+                        <div className="mt-3">
+                            <p className="font-bold font-sans">
+                                {truncateName(doctor.fullname)}
+                            </p>
+                            <div className="flex gap-2 my-1.5">
+                                <Image
+                                    src={stetoskopPurple}
+                                    width={15}
+                                    height={15}
+                                    alt="stetoskop.png"
+                                />
+                                <p className="text-xs text-primary font-semibold cursor-default">
+                                    {doctor.specialist}
+                                </p>
+                            </div>
+                            <button className="border-2 border-primary hover:bg-primary hover:text-white rounded-full py-1 px-4 text-xs font-semibold mt-3 transition duration-[.3s] ease-linear">
+                                Lihat Jadwal
+                            </button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
 }
--- src/components/views/Beranda/HomeQualifiedDoctor/index.jsx
+++ src/components/views/Beranda/HomeQualifiedDoctor/index.jsx
@@ -1,10 +1,27 @@
 import { useRouter } from 'next/router';
 import { Button } from '@nextui-org/react';
 import CardQualifiedDoctor from '@/components/ui/Card/CardQualifiedDoctors.jsx';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import doctorService from '@/services/user/doctor';
 
 export default function HomeQualifiedDoctors() {
+  const [doctors, setDoctors] = useState([]);
   const router = useRouter();
+
+  const fetchDoctors = async () => {
+    try {
+      const { data } = await doctorService.getDoctors();
+      setDoctors(data.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
+
+  console.log(doctors);
 
   const handleClickCheckDoctor = () => {
     router.push({
@@ -14,23 +31,25 @@ export default function HomeQualifiedDoctors() {
 
   return (
     <div className="flex flex-row-reverse max-[1070px]:flex-col justify-evenly items-center gap-4 bg-gradient-radial w-full h-max rounded-md px-1.5 my-2 py-12">
-      <div className=" flex flex-col justify-center max-[555px]:items-center max-[555px]:text-center  ">
+      <div className="flex flex-col justify-center max-[555px]:items-center max-[555px]:text-center">
         <h1 className="text-xl font-bold text-primary mb-3">Qualified Doctors</h1>
-        <h2 className="max-[555px]:text-3xl text-6xl font-bold text-slate-800 font-sans ">
+        <h2 className="max-[555px]:text-3xl text-6xl font-bold text-slate-800 font-sans">
           Keep doctors and <br /> loved ones in the
         </h2>
-        <p className="font-semibold text-slate-600 mt-5 max-[320px]:text-xs max-[460px]:hidden ">
+        <p className="font-semibold text-slate-600 mt-5 max-[320px]:text-xs max-[460px]:hidden">
           Security share your comprehensive medical history with <br /> doctors and loved ones, for better communication and <br /> care.
         </p>
         <Button
           className="w-max border-2 border-primary rounded-full my-4"
-          onClick={() => handleClickCheckDoctor()}
+          onClick={handleClickCheckDoctor}
         >
           Check Doctors
         </Button>
       </div>
       <div className="flex justify-center items-center max-[575px]:w-[220px] max-[759px]:w-[500px] min-[760px]:w-[500px] h-full gap-3">
-        <CardQualifiedDoctor />
+          <CardQualifiedDoctor doctors={doctors}/>
+        {/* {doctors.slice(0, 3).map((doctor, index) => (
+        ))} */}
       </div>
     </div>
   );
